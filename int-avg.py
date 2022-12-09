45a1477f314f88e4588ad37119909588c2583f42
shejialuo <shejialuo@gmail.com>
2022-12-09 01:51:28 -0500

    Finish Lab 3 Exercise 1

--- int-avg.py
+++ int-avg.py
@@ -19,7 +19,7 @@ b = z3.BitVec('b', 32)
 ##
 ## Use z3.UDiv(x, y) for unsigned division of x by y.
 ## Use z3.LShR(x, y) for unsigned (logical) right shift of x by y bits.
-u_avg = z3.UDiv(a + b, 2)
+u_avg = z3.UDiv(a, 2) + z3.UDiv(b, 2) + z3.UDiv(a % 2 + b % 2, 2)
 s_avg = (a + b) / 2
 
 ## Do not change the code below.
